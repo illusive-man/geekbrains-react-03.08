b1fcc99a015888a8c6b931d7c854f331184f7fa9
illusive-man <goujon@mail.ru>
2018-09-03 18:35:29 +0300

    Mopping up before restart

--- Serge_Kanashin/src/module.js
+++ Serge_Kanashin/src/module.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -20,20 +19,3 @@ appRoot.addEventListener('click', (e) => {
 });
 
 ReactDOM.render(template, appRoot);
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-console.log('React module is up and running!');
-
-let template = <h3>Developer: Serge Kanashin.<br /><br />
-               This info is from external React module. CLICK ME!</h3>;
-const appRoot = document.getElementById('app');
-
-appRoot.addEventListener('click', (e) => {
-    e.stopImmediatePropagation();
-    document.body.innerHTML += `<h2>${new Date().toLocaleDateString("ru-RU")}</h2>`;
-});
-
-ReactDOM.render(template, appRoot);
->>>>>>> upstream/master
